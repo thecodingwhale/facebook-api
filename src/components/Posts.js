2d962bca3a9742a5ee17376a5626a551c296ae22
aldren.terante <me@thecodingwhale.com>
2015-10-25 15:10:06 +0800

    infinite scroll is now working :)
    - but still need to optimize the js

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,58 +1,82 @@
 import React from 'react';
 import request from 'superagent';
 import Promise from 'promise';
+import Infinite from 'react-infinite';
+
+var ListItem = React.createClass({
+    render() {
+        var post = this.props.post;
+        return <div className="well post">
+            <div className="post-id">Id: <strong>{post.id}</strong></div>
+            <div className="post-title">Title: <strong>{post.title}</strong></div>
+            <div className="post-description">Description: <em>{post.description}</em></div>
+            <div className="post-date">{post.date}</div>
+        </div>;
+    }
+});
 
 export default React.createClass({
-    getInitialState() {
-        return {
-            posts: []
-        };
-    },
-    componentDidMount() {
+    getPosts(page, callback) {
         let promise = new Promise((resolve, reject) => {
             request
-               .get('/post/getPosts/11')
+               .get('/post/getPosts/' + page)
                .end((err, res) => {
                     if (err) {
                         reject(err);
                     } else {
-                        this.setState({
-                            posts: res.body
-                        });
+                        callback(res.body);
                         resolve(res);
                     }
                });
         });
     },
-    render() {
-        var postList = this.state.posts.map((post) => {
-            return<div className="well post" key={post.post_id}>
-                <div className="post-title">Id: <strong>{post.id}</strong></div>
-                <div className="post-title">Title: <strong>{post.title}</strong></div>
-                <div className="post-description">Description: <em>{post.description}</em></div>
-                <div className="post-date">{post.date}</div>
-            </div>
+    componentDidMount() {
+        this.getListPosts();
+    },
+    getListPosts() {
+        var self = this;
+        this.setState({
+            isInfiniteLoading: true
         });
-
-        var classPosts = 'posts hide';
-        if (Object.keys(this.state.posts).length > 1) {
-            classPosts += 'posts';
-        }
-
-        var classWell = 'well well-sm';
-        if (Object.keys(this.state.posts).length > 1) {
-            classWell += 'well well-sm hide';
-        }
-
-        return (
-            <div>
-                <div id="ajax-spinner" className={classWell}>
-                    <i className="fa fa-spinner fa-spin fa-lg"></i> Getting All Posts
-                </div>
-                <div className={classPosts}>
-                    {postList}
-                </div>
-            </div>
-        );
+        self.getPosts(this.state.page, (posts) => {
+            self.setState({
+                isInfiniteLoading: false,
+                page: self.state.page += 1,
+                elements: self.state.elements.concat(self.prepareListPost(posts))
+            });
+        });
+    },
+    getInitialState() {
+        return {
+            elements: [],
+            page: 1,
+            isInfiniteLoading: false
+        };
+    },
+    prepareListPost(posts) {
+        var elements = [];
+        posts.map((post) => {
+            elements.push(<ListItem key={post.post_id} post={post}/>)
+        });
+        return elements;
+    },
+    handleInfiniteLoad() {
+        this.getListPosts();
+    },
+    ajaxLoader() {
+        return <div id="ajax-spinner" className="well">
+            <i className="fa fa-spinner fa-spin fa-lg"></i> Getting All Posts
+        </div>;
+    },
+    render() {
+        return <Infinite    elementHeight={160}
+                            useWindowAsScrollContainer={true}
+                            infiniteLoadBeginBottomOffset={200}
+                            onInfiniteLoad={this.handleInfiniteLoad}
+                            loadingSpinnerDelegate={this.ajaxLoader()}
+                            isInfiniteLoading={this.state.isInfiniteLoading}
+                            >
+            {this.state.elements}
+        </Infinite>;
     }
 });
